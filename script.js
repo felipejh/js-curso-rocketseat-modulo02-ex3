bb160b11472f272923457b2c85b6161ed4179792
Felipe Hoffmann <felipehoffmann@MacBook-Pro-de-Felipe.local>
2020-01-13 22:16:15 -0300

    First commit

---
+++ script.js
@@ -0,0 +1,18 @@
+﻿var nomes = ["Diego", "Gabriel", "Lucas"];
+
+var listElement = document.createElement("ul");
+var body = document.body;
+
+function adicionaItem() {
+
+    for (nome of nomes) {
+        var itemListElement = document.createElement("li");
+        var textNome = document.createTextNode(nome);
+
+        itemListElement.appendChild(textNome);
+        listElement.appendChild(itemListElement);
+    }
+}
+
+adicionaItem();
+body.appendChild(listElement);
